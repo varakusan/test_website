451255069020b7b99f474e2db1b7d99cbac92ecc
varakusan <varakusan@gmail.com>
2025-08-18 20:32:41 +0000

    Deploying to gh-pages from @ varakusan/test_website@4eb540da7358a7de2b711985926c26d933777719 🚀

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// Using a relative base helps previews on GitHub Pages work without knowing repo name
-export default defineConfig({
-  plugins: [react()],
-  base: './'
-})
